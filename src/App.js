0b0aa0fe4eeabf4ac86b0ebb9f9fd2cee0203f01
sumnesha47 <sumnesha47@gmail.com>
2022-03-28 19:42:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { SearchOutlined, CloseOutlined } from "@ant-design/icons";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [compniesData, setCompniesData] = useState([]);
+  const [selectedData, setSelectedData] = useState({});
+
+  const handleChange = (e) => {
+    setSelectedData({});
+    setSearchText(e.target.value);
+    const newCompniesData = data.filter((company) => {
+      return company.company.toLowerCase().includes(searchText);
+    });
+    if (e.target.value === "") {
+      setCompniesData([]);
+    } else {
+      setCompniesData(newCompniesData);
+    }
+  };
+
+  const handleClick = (company) => {
+    setSearchText("");
+    setCompniesData("");
+    setSelectedData(company);
+  };
+
+  const handleClearBtnClick = () => {
+    setSearchText("");
+    setCompniesData([]);
+    setSelectedData({});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <input
+            type={"text"}
+            value={searchText}
+            onChange={handleChange}
+            placeholder={"Search Company Here..."}
+          />
+          <div className="icon">
+            {searchText ? (
+              <CloseOutlined
+                onClick={handleClearBtnClick}
+                className="clear-icon"
+              />
+            ) : (
+              <SearchOutlined />
+            )}
+          </div>
+        </div>
+        {compniesData.length !== 0 && (
+          <div className="data">
+            {compniesData.map((company) => {
+              return (
+                <div
+                  onClick={() => handleClick(company)}
+                  className="data-item"
+                  key={company._id}
+                >
+                  {company.company}
+                </div>
+              );
+            })}
+          </div>
+        )}
 
+        {Object.keys(selectedData).length !== 0 && (
+          <div className="card">
+            <h2>
+              Company Name: <span>{selectedData.company}</span>
+            </h2>
+            <h2>
+              Company Address: <span>{selectedData.address}</span>
+            </h2>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 export default App;
---
+++ src/data.js
@@ -0,0 +1,252 @@
+export default [
+  {
+    _id: "62415f2ea7c33ff3d5d7f69e",
+    company: "GENEKOM",
+    address: "523 Logan Street, Longbranch, Alaska, 2942",
+  },
+  {
+    _id: "62415f2e92562c2a1024d175",
+    company: "DUFLEX",
+    address: "839 Hubbard Place, Churchill, Montana, 9306",
+  },
+  {
+    _id: "62415f2e512a809c1336657e",
+    company: "COWTOWN",
+    address: "904 Corbin Place, Bancroft, Virgin Islands, 2494",
+  },
+  {
+    _id: "62415f2e796db5592c966f14",
+    company: "COGENTRY",
+    address: "831 McKibbin Street, Riceville, Washington, 265",
+  },
+  {
+    _id: "62415f2e79d50c5a6db51386",
+    company: "ECOLIGHT",
+    address: "547 Alabama Avenue, Kenmar, Oregon, 6357",
+  },
+  {
+    _id: "62415f2e862ec1e818eca0f2",
+    company: "INSURETY",
+    address: "861 Olive Street, Avalon, Utah, 4348",
+  },
+  {
+    _id: "62415f2e96810134965fa51b",
+    company: "SEALOUD",
+    address: "567 Waldane Court, Newry, Federated States Of Micronesia, 1170",
+  },
+  {
+    _id: "62415f2e08bd5c89c4b5d56c",
+    company: "CORPULSE",
+    address: "964 Blake Avenue, Dragoon, Oklahoma, 3718",
+  },
+  {
+    _id: "62415f2e344695be603e114d",
+    company: "ECOSYS",
+    address: "684 Raleigh Place, Snelling, South Carolina, 5723",
+  },
+  {
+    _id: "62415f2ec555efb3b6228eab",
+    company: "ZILLADYNE",
+    address: "449 Ash Street, Dellview, Palau, 9866",
+  },
+  {
+    _id: "62415f2e5dfdf3185d1cdeaf",
+    company: "MITROC",
+    address: "633 Calder Place, Magnolia, Pennsylvania, 1121",
+  },
+  {
+    _id: "62415f2ef98abace15a7260d",
+    company: "TECHTRIX",
+    address: "849 Montague Street, Reno, Nevada, 7084",
+  },
+  {
+    _id: "62415f2e6270c452bf40058e",
+    company: "DEVILTOE",
+    address: "311 Fountain Avenue, Dunnavant, Alabama, 1714",
+  },
+  {
+    _id: "62415f2e219805aab251664e",
+    company: "ENERSOL",
+    address: "683 Legion Street, Hartsville/Hartley, North Dakota, 370",
+  },
+  {
+    _id: "62415f2ee958b0e2f02ae30d",
+    company: "SYBIXTEX",
+    address: "422 Elton Street, Ellerslie, Rhode Island, 533",
+  },
+  {
+    _id: "62415f2eaa946054112210cb",
+    company: "ESSENSIA",
+    address: "478 Ryder Street, Leeper, Indiana, 2624",
+  },
+  {
+    _id: "62415f2eac9971af3522f329",
+    company: "SUSTENZA",
+    address: "360 Granite Street, Onton, Wyoming, 7336",
+  },
+  {
+    _id: "62415f2ef8450ba32c3f2cf8",
+    company: "GAZAK",
+    address: "103 Colby Court, Winesburg, Iowa, 7922",
+  },
+  {
+    _id: "62415f2ed516a9826d7e7dfd",
+    company: "ZAPPIX",
+    address: "560 Bokee Court, Glendale, Missouri, 2621",
+  },
+  {
+    _id: "62415f2ed29bc0b1c196ebd4",
+    company: "XPLOR",
+    address: "882 Onderdonk Avenue, Clarksburg, Georgia, 4837",
+  },
+  {
+    _id: "62415f2e44852fbb80d0a2fa",
+    company: "ENERVATE",
+    address: "698 Harkness Avenue, Concho, New Hampshire, 7064",
+  },
+  {
+    _id: "62415f2ec9cac4ca191b75dd",
+    company: "GOLOGY",
+    address: "918 National Drive, Libertytown, Arkansas, 3461",
+  },
+  {
+    _id: "62415f2ecfe2cb4e5eaca310",
+    company: "INTERGEEK",
+    address: "997 Butler Place, Ahwahnee, North Carolina, 7018",
+  },
+  {
+    _id: "62415f2e895370edfa1892e5",
+    company: "MICRONAUT",
+    address: "417 Bancroft Place, Fairlee, Vermont, 6666",
+  },
+  {
+    _id: "62415f2e8d80d66f2924b18f",
+    company: "BISBA",
+    address: "265 Monument Walk, Coinjock, Florida, 6904",
+  },
+  {
+    _id: "62415f2ed4e3f45a1e79a21e",
+    company: "HAIRPORT",
+    address: "511 Jackson Street, Brecon, Arizona, 4840",
+  },
+  {
+    _id: "62415f2e4c2c85bfe70efa88",
+    company: "ISONUS",
+    address: "357 Sedgwick Street, Westmoreland, Tennessee, 8984",
+  },
+  {
+    _id: "62415f2e371e81e90eff7d57",
+    company: "EXOPLODE",
+    address: "804 Walker Court, Echo, Massachusetts, 1172",
+  },
+  {
+    _id: "62415f2ec743ff930b2aa963",
+    company: "PLASMOX",
+    address: "641 Hampton Place, Convent, South Dakota, 3052",
+  },
+  {
+    _id: "62415f2efc24ac115b0ad6e2",
+    company: "OBLIQ",
+    address: "236 Madison Place, Genoa, Delaware, 7429",
+  },
+  {
+    _id: "62415f2e5b6c92b003d77c40",
+    company: "SYNKGEN",
+    address: "425 Miller Avenue, Catharine, New York, 8411",
+  },
+  {
+    _id: "62415f2e3611e7854ff1bae7",
+    company: "DELPHIDE",
+    address: "275 Stockton Street, Lupton, Ohio, 832",
+  },
+  {
+    _id: "62415f2e064fbf96ee2df23d",
+    company: "REMOLD",
+    address: "231 Lott Place, Harviell, Kentucky, 7347",
+  },
+  {
+    _id: "62415f2eb4b08dc71105eed8",
+    company: "QIAO",
+    address: "325 Dikeman Street, Lookingglass, Idaho, 1059",
+  },
+  {
+    _id: "62415f2e5572dee4291cb2a3",
+    company: "QUARMONY",
+    address: "237 Lamont Court, Enlow, New Jersey, 6024",
+  },
+  {
+    _id: "62415f2ee5b884dac83400af",
+    company: "MATRIXITY",
+    address: "872 Wilson Avenue, Mammoth, Hawaii, 9238",
+  },
+  {
+    _id: "62415f2e5da6189f2fc5c931",
+    company: "VOIPA",
+    address: "944 Hicks Street, Cade, Louisiana, 7004",
+  },
+  {
+    _id: "62415f2ebb4f1ccdfeb69362",
+    company: "NORSUP",
+    address: "824 Ridgewood Place, Shelby, New Mexico, 5631",
+  },
+  {
+    _id: "62415f2e1997ffa2fd05662f",
+    company: "PYRAMI",
+    address: "234 Kensington Walk, Barclay, California, 9617",
+  },
+  {
+    _id: "62415f2ea0ee49c3f894e744",
+    company: "SATIANCE",
+    address: "660 Bevy Court, Vale, West Virginia, 1012",
+  },
+  {
+    _id: "62415f2e7f612bca7e29d9c7",
+    company: "QUALITERN",
+    address: "695 Kingsway Place, Camptown, Minnesota, 7493",
+  },
+  {
+    _id: "62415f2e1f44e4d94f95f77c",
+    company: "PYRAMIA",
+    address: "906 Richards Street, Odessa, Michigan, 156",
+  },
+  {
+    _id: "62415f2ed1fd386bee5a3445",
+    company: "VENDBLEND",
+    address: "813 Mill Lane, Connerton, Wisconsin, 6392",
+  },
+  {
+    _id: "62415f2ee853373140f083f3",
+    company: "KEEG",
+    address: "903 Hutchinson Court, Joppa, Mississippi, 9510",
+  },
+  {
+    _id: "62415f2ecff4a6d3be484c30",
+    company: "ACCRUEX",
+    address: "888 Clifton Place, Fairfield, Connecticut, 9650",
+  },
+  {
+    _id: "62415f2e4d04c8d8db8f7821",
+    company: "IMANT",
+    address: "774 Hampton Avenue, Klondike, Kansas, 6639",
+  },
+  {
+    _id: "62415f2ea016854a4dfbf0b8",
+    company: "ISOTRONIC",
+    address: "969 Farragut Road, Noblestown, District Of Columbia, 1338",
+  },
+  {
+    _id: "62415f2ea5fdc4e164704e99",
+    company: "ZIDOX",
+    address: "892 Linden Boulevard, Makena, Virginia, 1655",
+  },
+  {
+    _id: "62415f2ef802d7e55f942a6d",
+    company: "XINWARE",
+    address: "743 Celeste Court, Elrama, Texas, 9084",
+  },
+  {
+    _id: "62415f2e820b6520d4d1aa8f",
+    company: "ZYTREK",
+    address: "423 Kosciusko Street, Swartzville, Marshall Islands, 9601",
+  },
+];
