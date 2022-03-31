0293592c47b8d13c181fc209fc3e8e9d2dc3cce1
sumnesha47 <sumnesha47@gmail.com>
2022-04-01 01:15:44 +0530

    api addition

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { SearchOutlined, CloseOutlined } from "@ant-design/icons";
-import data from "./data";
+
+const url = "https://72ih8opnm2.execute-api.ap-south-1.amazonaws.com/live";
 
 const App = () => {
+  const [data, setData] = useState({});
   const [searchText, setSearchText] = useState("");
   const [compniesData, setCompniesData] = useState([]);
   const [selectedData, setSelectedData] = useState({});
 
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const cdata = await response.json();
+    setData(cdata);
+    setCompniesData(cdata.slice());
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const handleChange = (e) => {
-    setSelectedData({});
     setSearchText(e.target.value);
-    const newCompniesData = data.filter((company) => {
-      return company.company.toLowerCase().includes(searchText);
+
+    const newCompniesData = compniesData.filter((company) => {
+      return company.company_name
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
     });
     if (e.target.value === "") {
-      setCompniesData([]);
+      setCompniesData(data.slice());
     } else {
       setCompniesData(newCompniesData);
     }
@@ -23,14 +37,22 @@ const App = () => {
 
   const handleClick = (company) => {
     setSearchText("");
-    setCompniesData("");
+    setCompniesData(data.slice());
     setSelectedData(company);
   };
 
   const handleClearBtnClick = () => {
     setSearchText("");
-    setCompniesData([]);
+    setCompniesData(data.slice());
     setSelectedData({});
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.code === "Backspace" || event.code === "Delete") {
+      setCompniesData(data.slice());
+    }
+    if (event === "ArrowDown") {
+    }
   };
 
   return (
@@ -42,6 +64,7 @@ const App = () => {
             value={searchText}
             onChange={handleChange}
             placeholder={"Search Company Here..."}
+            onKeyDown={handleKeyDown}
           />
           <div className="icon">
             {searchText ? (
@@ -54,16 +77,17 @@ const App = () => {
             )}
           </div>
         </div>
-        {compniesData.length !== 0 && (
+        {searchText !== "" && (
           <div className="data">
             {compniesData.map((company) => {
+              const { company_name, location } = company;
               return (
                 <div
                   onClick={() => handleClick(company)}
                   className="data-item"
                   key={company._id}
                 >
-                  {company.company}
+                  {company_name}
                 </div>
               );
             })}
@@ -73,10 +97,16 @@ const App = () => {
         {Object.keys(selectedData).length !== 0 && (
           <div className="card">
             <h2>
-              Company Name: <span>{selectedData.company}</span>
+              Company Name: <span>{selectedData.company_name}</span>
             </h2>
             <h2>
-              Company Address: <span>{selectedData.address}</span>
+              Company Address:
+              <span>
+                {selectedData.location.replace(
+                  /[`~!@#$%^&*()_|+\-=?;:'".<>{}[\]\\/]/gi,
+                  ""
+                )}
+              </span>
             </h2>
           </div>
         )}
